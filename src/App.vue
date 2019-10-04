c3d0dfa3f1eced303fe5172b39601dc7aba028b2
Ana DIACONU <adiaconu@pentalog.com>
2019-10-04 16:20:06 +0300

    Added custom header component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,17 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
+    <CustomHeader />
     <v-content>
-      <HelloWorld/>
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import CustomHeader from "./components/header/TheHeader.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+  name: "App",
+  components: { CustomHeader }
 };
 </script>
---
+++ src/components/header/TheHeader.vue
@@ -0,0 +1,9 @@
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss"></style>
+
+<script>
+
+export default {
+  name: 'custom-header',
+};
+</script>
