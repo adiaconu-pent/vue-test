1053c31b3db3fc42a84edb5fd780b61ef3459ef7
Ana DIACONU <adiaconu@pentalog.com>
2019-10-04 14:13:38 +0300

    Implemeted axios, implemented userService to get users on the users page

---
+++ src/services/users.js
@@ -0,0 +1,7 @@
+import instance from './base';
+
+const resource = '/users'
+
+export default {
+    getUsers: instance.get(`${resource}`)
+}
---
+++ src/views/usersPage/Users.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="users">
+    <h1>This is the page wit users list.</h1>
+  </div>
+</template>
+
+<script>
+import userService from "@/services/users";
+
+export default {
+  name: "users",
+  data() {
+    return {
+      loading: false,
+      users: []
+    };
+  },
+  created() {
+    this.getUsers();
+  },
+  methods: {
+    getUsers() {
+      this.loading = true;
+      userService.getUsers
+        .then(response => {
+          this.loading = false;
+          this.users = response.data;
+        })
+        .catch(error => {
+          this.loading = false;
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
